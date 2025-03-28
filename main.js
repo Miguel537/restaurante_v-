83556d573706bbaeebbbd04c490d6f810ec77a9e
Miguel Joaquim <migueljoaquim409@gmail.com>
2025-03-28 19:42:30 -0300

    Commit restaurante site

--- main.js
+++ main.js
@@ -15,12 +15,12 @@ botoesAdicionar.forEach(botao => {
 
         // Adiciona item à lista
         const itemPedido = document.createElement('li');
-        itemPedido.textContent = `${nome} - R$ ${preco.toFixed(2)}`;
+        itemPedido.textContent = ${nome} - R$ ${preco.toFixed(2)};
         listaPedido.appendChild(itemPedido);
 
         // Atualiza o total
         total += preco;
-        totalElemento.textContent = `Total do Pedido: R$ ${total.toFixed(2)}`;
+        totalElemento.textContent = Total do Pedido: R$ ${total.toFixed(2)};
     });
 });
 
@@ -29,7 +29,7 @@ const botaoFinalizarPedido = document.getElementById('finalizar-pedido');
 
 botaoFinalizarPedido.addEventListener('click', () => {
     if (total > 0) {
-        alert(`Pedido realizado com sucesso! Total: R$ ${total.toFixed(2)}`);
+        alert(Pedido realizado com sucesso! Total: R$ ${total.toFixed(2)});
     } else {
         alert('Nenhum item foi adicionado ao pedido.');
     }
